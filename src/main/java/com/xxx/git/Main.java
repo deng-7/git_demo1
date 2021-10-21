86e8a7ff6ba68546e0ae352a3572f33aa6e1e116
deng-7 <1522764046@qq.com>
2021-10-21 23:07:11 +0800

    hello3 -2

--- src/main/java/com/xxx/git/Main.java
+++ src/main/java/com/xxx/git/Main.java
@@ -9,6 +9,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello 1");
         System.out.println("hello 2");
-        System.out.println("hello 3");
     }
 }
