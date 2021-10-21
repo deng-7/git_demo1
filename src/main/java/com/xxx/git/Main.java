9207fd3cac2f8a30efc711c685e12f4f00c73f45
deng-7 <1522764046@qq.com>
2021-10-21 23:02:21 +0800

    提交1

--- src/main/java/com/xxx/git/Main.java
+++ src/main/java/com/xxx/git/Main.java
@@ -7,6 +7,7 @@ package com.xxx.git;
  */
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("hello 1");
+        System.out.println("hello 2");
     }
 }
